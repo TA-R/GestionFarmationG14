0bdbed1f025d728c4266e906d59a0edceb51521a
fatis1403 <fatishseine@gmail.com>
2026-06-16 21:28:08 +0100

    Add home page and formations components

--- src/components/formations/FormationCard.jsx
+++ src/components/formations/FormationCard.jsx
@@ -1,1 +1,20 @@
+function FormationCard({ formation }) {
+  return (
+    <article className="formation-card">
+      <div className="formation-card__header">
+        <span className="formation-card__category">{formation.category}</span>
+        <span className="formation-card__level">{formation.level}</span>
+      </div>
 
+      <h3>{formation.title}</h3>
+      <p>{formation.description}</p>
+
+      <div className="formation-card__footer">
+        <span>{formation.duration}</span>
+        <button type="button">Voir details</button>
+      </div>
+    </article>
+  )
+}
+
+export default FormationCard
